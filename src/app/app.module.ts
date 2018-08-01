6433376feb3d8c3c257ef20c81cf067319e99f42
“yannyyss” <yannyyss@hotmail.com>
2018-08-01 09:25:58 -0500

    started this great project that is going to win lol made by super yanely

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
+// Components:
 import { AppComponent } from './app.component';
+import { routes } from "./routes";
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { MapComponent } from './map/map.component';
+import { ProfilesComponent } from './profiles/profiles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    ProfileComponent,
+    HomeComponent,
+    MenuComponent,
+    MapComponent,
+    ProfilesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/routes.ts
@@ -0,0 +1,20 @@
+import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { MenuComponent } from './menu/menu.component';
+import { MapComponent } from './map/map.component';
+import { ProfilesComponent } from './profiles/profiles.component';
+
+export const routes: Routes = [
+
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: AppComponent },
+    { path: 'signup', component: SignupComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'profile', component: ProfileComponent },
+    { path: 'menu', component: MenuComponent },
+    { path: 'map', component: MapComponent },
+    { path: 'profiles', component: ProfilesComponent }
+];
